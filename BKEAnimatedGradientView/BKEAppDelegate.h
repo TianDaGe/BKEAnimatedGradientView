ec0cc2d6303d51e841aaf28411484833ec5bdecb
Brian Kenny <brian@bkenny.com>
2014-02-03 17:35:26 +0000

    Initial Commit

---
+++ BKEAnimatedGradientView/BKEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BKEAppDelegate.h
+//  BKEAnimatedGradientView
+//
+//  Created by Brian Kenny on 03/02/2014.
+//  Copyright (c) 2014 Brian Kenny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BKEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BKEAnimatedGradientView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BKEAnimatedGradientView
+//
+//  Created by Brian Kenny on 03/02/2014.
+//  Copyright (c) 2014 Brian Kenny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BKEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BKEAppDelegate class]));
+    }
+}
